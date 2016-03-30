810eb1ec94c77705a58be76a7099c484cbc5ff6b
Jim Laney <jlaney@uw.edu>
2016-03-30 16:09:42 -0700

    use default subject guide urls

--- libguide/views.py
+++ libguide/views.py
@@ -6,16 +6,14 @@ from django.core.context_processors import csrf
 from blti import BLTI, BLTIException
 from sis_provisioner.policy import CoursePolicy, CoursePolicyException
 from restclients.library.currics import (
-    get_subject_guide_for_canvas_course_sis_id)
+    get_subject_guide_for_canvas_course_sis_id, get_default_subject_guide)
 from restclients.exceptions import DataFailureException
 
 
 @csrf_exempt
 def LibGuide(request, template='libguide/libguide.html'):
     blti_data = {"context_label": "NO COURSE"}
-    validation_error = None
     status_code = 200
-    generic_guide_url = '2000-autumn-NONE-000-A'
     params = {}
 
     try:
@@ -31,29 +29,27 @@ def LibGuide(request, template='libguide/libguide.html'):
         subaccount_id = blti_data.get('custom_canvas_account_sis_id')
         blti.set_session(request, user_id=canvas_login_id)
 
-        params['subaccount_id'] = subaccount_id
-        params['sis_course_id'] = sis_course_id
-        params['canvas_course_id'] = canvas_course_id
+        if 'uwcourse:tacoma' in subaccount_id:
+            campus = 'tacoma'
+        elif 'uwcourse:bothell' in subaccount_id:
+            campus = 'bothell'
+        else:
+            campus = 'seattle'
 
         try:
             CoursePolicy().valid_academic_course_sis_id(sis_course_id)
-        except CoursePolicyException as err:
-            sis_course_id = generic_guide_url
 
-        try:
             subject_guide = get_subject_guide_for_canvas_course_sis_id(
                 sis_course_id)
-        except DataFailureException as err:
-            if err.status == 404:
-                subject_guide = get_subject_guide_for_canvas_course_sis_id(
-                    generic_guide_url)
-            else:
-                raise
 
-        # Boolean is nice here
-        subject_guide.has_discipline = True if (
-            subject_guide.discipline != 'your discipline') else False
+            if (subject_guide.is_default_guide and
+                    subject_guide.default_guide_campus.lower() != campus):
+                subject_guide = get_default_subject_guide(campus=campus)
 
+        except CoursePolicyException as err:
+            subject_guide = get_default_subject_guide(campus=campus)
+
+        params['campus'] = campus
         params['subject_guide'] = subject_guide
 
     except BLTIException as err:
