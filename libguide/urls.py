4c7b8084944200641349a1a9f308325e88f205b8
Jim Laney <jlaney@uw.edu>
2017-05-15 15:37:26 -0700

    drop patterns from urls.py

--- libguide/urls.py
+++ libguide/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url
 from libguide.views import LibGuideView
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', LibGuideView.as_view()),
-)
+]
