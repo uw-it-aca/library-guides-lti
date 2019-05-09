f9c28a5a815c7dcf370c7bce3ce3fd67b58b9aad
Jim Laney <jlaney@uw.edu>
2019-05-09 10:51:28 -0700

    pin django <2.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=['libguide'],
     include_package_data=True,
     install_requires = [
-        'Django>=2.1',
+        'Django>=2.1,<2.2',
         'django-blti>=2.1.2',
         'UW-RestClients-Core>=1.1.1,<2.0',
         'UW-RestClients-Libraries>=1.0,<2.0',
