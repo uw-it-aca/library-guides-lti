c1c5dad002c932cf5b50c28bd84fddc76e51ef95
Jim Laney <jlaney@uw.edu>
2020-09-02 09:58:45 -0700

    ignore vendor files for jshint

--- libguide/cache.py
+++ libguide/cache.py
@@ -4,4 +4,5 @@ from rc_django.cache_implementation.memcache import MemcachedCache
 
 class LibCurricsCache(MemcachedCache):
     def get_cache_expiration_time(self, service, url):
-        return getattr(settings, 'LIBCURRICS_CACHE_EXPIRES', 60 * 60 * 4)
+        if 'libcurrics' == service:
+            return getattr(settings, 'LIBCURRICS_CACHE_EXPIRES', 60 * 60 * 4)
