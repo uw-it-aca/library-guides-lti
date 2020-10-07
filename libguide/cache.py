b6658e63a69db590093e36570b81dec79a3fa89c
Jim Laney <jlaney@uw.edu>
2020-10-07 10:12:22 -0700

    updates cache backend to pymemcache

--- libguide/cache.py
+++ libguide/cache.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from rc_django.cache_implementation.memcache import MemcachedCache
+from memcached_clients import RestclientPymemcacheClient
 
 
-class LibCurricsCache(MemcachedCache):
-    def get_cache_expiration_time(self, service, url):
+class LibCurricsCache(RestclientPymemcacheClient):
+    def get_cache_expiration_time(self, service, url, status=None):
         if 'libcurrics' == service:
             return getattr(settings, 'LIBCURRICS_CACHE_EXPIRES', 60 * 60 * 4)
