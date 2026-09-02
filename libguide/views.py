389146d366cb0bc0ac6637093251022dd2d880b2
Jim Laney <jlaney@uw.edu>
2026-09-02 08:44:20 -0700

    ruff fixes

--- libguide/views.py
+++ libguide/views.py
@@ -1,10 +1,11 @@
 # Copyright 2026 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from django.conf import settings
 from blti.views import BLTILaunchView
-from libguide.dao.library import campus_from_subaccount, get_subject_guide
+from django.conf import settings
 from restclients_core.exceptions import DataFailureException
+
+from libguide.dao.library import campus_from_subaccount, get_subject_guide
 
 
 class LibGuideView(BLTILaunchView):
@@ -18,7 +19,7 @@ class LibGuideView(BLTILaunchView):
         if self.blti.course_sis_id:
             course_sis_id = self.blti.course_sis_id
         else:
-            course_sis_id = 'course_{}'.format(self.blti.canvas_course_id)
+            course_sis_id = f'course_{self.blti.canvas_course_id}'
 
         campus = campus_from_subaccount(self.blti.account_sis_id)
 
@@ -27,6 +28,6 @@ class LibGuideView(BLTILaunchView):
             return {'campus': campus, 'subject_guide': subject_guide}
 
         except DataFailureException as err:
-            return {'error': (
-                'UW Libraries Subject Guides are not available: {}'.format(
-                    err.msg))}
+            return {
+                'error': f'UW Libraries Subject Guides are not available: {err.msg}'
+            }
